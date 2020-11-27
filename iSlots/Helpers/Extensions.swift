1aedd7de1e9c5ed0fcaad8ada37db3961c184da4
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-27 12:14:58 -0800

    Add assets and build headerView

---
+++ iSlots/Helpers/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  iSlots
+//
+//  Created by Jonathan Sweeney on 11/27/20.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}
---
+++ iSlots/Helpers/Modifiers.swift
@@ -0,0 +1,43 @@
+//
+//  Modifiers.swift
+//  iSlots
+//
+//  Created by Jonathan Sweeney on 11/27/20.
+//
+
+import SwiftUI
+
+struct ShadowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color("ColorTransparentBlack"), radius: 0, x: 0, y: 6)
+    }
+}
+
+struct ButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .accentColor(Color.white)
+    }
+}
+
+struct ScoreNumberModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color("ColorTransparentBlack"), radius: 0, x: 0, y: 3)
+            .layoutPriority(1)
+    }
+}
+
+struct ScoreContainerModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.vertical, 4)
+            .padding(.horizontal, 16)
+            .background(
+                Capsule()
+                    .foregroundColor(Color("ColorTransparentBlack"))
+            )
+    }
+}

44213e509e2acb28e59f15e46e04e1cee434f55b
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-27 09:31:33 -0800

    Initial Commit

---
+++ iSlots/iSlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSlotsApp.swift
+//  iSlots
+//
+//  Created by Jonathan Sweeney on 11/27/20.
+//
+
+import SwiftUI
+
+@main
+struct iSlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
